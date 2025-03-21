810843781215df923e1ac01f6ec11eb8472f6fdf
teigorme <igorteixeira354@gmail.com>
2025-03-21 16:41:43 +0100

    create feat for recording and listen audio

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,37 @@
-import { View, Text, StatusBar } from "react-native";
+import { View, Text, StatusBar, TextInput, Alert } from "react-native";
 import React from "react";
+import Voice, { SpeechResultsEvent } from "@react-native-voice/voice";
 import { Button } from "@/components/button";
 
 const Index = () => {
+  const [isListening, setIsListening] = React.useState<boolean>(false);
+  const [search, setSearch] = React.useState<string>("");
+
+  function onSpeechResults({ value }: SpeechResultsEvent) {
+    console.warn(value);
+  }
+
+  async function Recording() {
+    try {
+      if (isListening) {
+        await Voice.stop();
+      }
+      setSearch("");
+      await Voice.start("pt-AO");
+      setIsListening(true);
+    } catch (error:any) {
+      Alert.alert("Erro no app",error)
+    }
+  }
+
+  React.useEffect(() => {
+    Voice.onSpeechResults = onSpeechResults;
+  }, []);
   return (
-    <View className="h-full bg-gray-700">
+    <View className="h-full bg-gray-200">
+      <TextInput onChangeText={setSearch} value={search} />
       <View className="flex-1 items-center justify-center px-4">
-        <Text className="text-gray-200 text-xl font-bold">
+        <Text className="text-gray-900 text-xl font-bold">
           Olá, Bem-vindo ao MoSom
         </Text>
         <Button
